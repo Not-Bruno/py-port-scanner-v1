7ec43a94f3188ca5a4d5d3170904e1367b747a39
Bruno <brunoscholz@gmx.de>
2023-01-17 10:08:44 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     print("-" * 50)
 
 
-    for i in range(int(p_start), int(p_end)):
+    for i in range(int(p_start), int(p_end)+1):
         sock = socket.socket()
 
         try:
